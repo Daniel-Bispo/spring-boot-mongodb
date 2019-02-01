28bd88da774e0ee41a3688af59405dac4cb191d8
Daniel Bispo <danielvbispo@outlook.com>
2019-02-01 13:14:06 -0200

    First commit

---
+++ src/main/java/com/danielbispo/springbootmongodb/SpringbootMongodbApplication.java
@@ -0,0 +1,15 @@
+package com.danielbispo.springbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringbootMongodbApplication.class, args);
+	}
+
+}
+
